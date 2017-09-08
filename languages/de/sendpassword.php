b55b488a6605ca6545b1998a4790e18b7cf1fb1b
Julian Knorr <git@jknorr.eu>
2017-09-09 00:42:05 +0200

    Use correct quotes for escape sequences

--- languages/de/sendpassword.php
+++ languages/de/sendpassword.php
@@ -12,9 +12,9 @@
  * @license			LGPL-3.0
  */
 
-$GLOBALS['TL_LANG']['sendpassword']['sendpassword_subject_frontend'] = 'Neues Kennwort für %s';
-$GLOBALS['TL_LANG']['sendpassword']['sendpassword_text_frontend'] = 'Hallo %s!\n\nDies sind Ihre neuen Zugangsdaten für %s:\n\nBenutzername: %s\nKennwort: %s\n\nBitte ändern Sie das Kennwort nach der ersten Anmeldung.\n\nDies ist eine automatisch generierte E-Mail! Bitte Antworten sie nicht auf diese E-Mail!';
-$GLOBALS['TL_LANG']['sendpassword']['sendpassword_subject_backend'] = 'Neues Kennwort für das Backend von %s';
-$GLOBALS['TL_LANG']['sendpassword']['sendpassword_text_backend'] = 'Hallo %s!\n\nDies sind Ihre neuen Zugangsdaten für das Contao Backend von %s:\n\nBenutzername: %s\nKennwort: %s\n\nBitte ändern Sie das Kennwort nach der ersten Anmeldung.\nAchtung: Dieses Kennwort entspricht nicht dem Kennwort für das Frontend!\n\nDies ist eine automatisch generierte E-Mail! Bitte Antworten sie nicht auf diese E-Mail!';
+$GLOBALS['TL_LANG']['sendpassword']['sendpassword_subject_frontend'] = "Neues Kennwort für %s";
+$GLOBALS['TL_LANG']['sendpassword']['sendpassword_text_frontend'] = "Hallo %s!\n\nDies sind Ihe neuen Zugangsdaten für %s:\n\nBenutzername: %s\nKennwort: %s\n\nBitte ändern Sie das Kennwort nach der ersten Anmeldung.\n\nDies ist eine automatisch generierte E-Mail! Bitte Antworten sie nicht auf diese E-Mail!";
+$GLOBALS['TL_LANG']['sendpassword']['sendpassword_subject_backend'] = "Neues Kennwort für das Backend von %s";
+$GLOBALS['TL_LANG']['sendpassword']['sendpassword_text_backend'] = "Hallo %s!\n\nDies sind Ihre neuen Zugangsdaten für das Contao Backend von %s:\n\nBenutzername: %s\nKennwort: %s\n\nBitte ändern Sie das Kennwort nach der ersten Anmeldung.\nAchtung: Dieses Kennwort entspricht nicht dem Kennwort für das Frontend!\n\nDies ist eine automatisch generierte E-Mail! Bitte Antworten sie nicht auf diese E-Mail!";
 
 ?>
--- languages/en/sendpassword.php
+++ languages/en/sendpassword.php
@@ -12,9 +12,9 @@
  * @license			LGPL-3.0
  */
 
-$GLOBALS['TL_LANG']['sendpassword']['sendpassword_subject_frontend'] = 'New password for %s';
-$GLOBALS['TL_LANG']['sendpassword']['sendpassword_text_frontend'] = 'Hello %s!\n\nHere are your new credentials for %s:\n\nUsername: %s\nPassword: %s\n\nPlease change your password after next login.\n\nThis message was automatically send! Please do not answer to this message!';
-$GLOBALS['TL_LANG']['sendpassword']['sendpassword_subject_backend'] = 'Hello %s!\n\nHere are your new credentials for Contao backend of %s:\n\nUsername: %s\nPassword: %s\n\nPlease change your password after next login.\nCaution: The passwords of back end and front end are independent from each other.\n\nThis message was automatically send! Please do not answer to this message!';
-$GLOBALS['TL_LANG']['sendpassword']['sendpassword_text_backend'] = 'New password for back end of %s';
+$GLOBALS['TL_LANG']['sendpassword']['sendpassword_subject_frontend'] = "New password for %s";
+$GLOBALS['TL_LANG']['sendpassword']['sendpassword_text_frontend'] = "Hello %s!\n\nHere are your new credentials for %s:\n\nUsername: %s\nPassword: %s\n\nPlease change your password after next login.\n\nThis message was automatically send! Please do not answer to this message!";
+$GLOBALS['TL_LANG']['sendpassword']['sendpassword_subject_backend'] = "New password for back end of %s";
+$GLOBALS['TL_LANG']['sendpassword']['sendpassword_text_backend'] = "Hello %s!\n\nHere are your new credentials for Contao backend of %s:\n\nUsername: %s\nPassword: %s\n\nPlease change your password after next login.\nCaution: The passwords of back end and front end are independent from each other.\n\nThis message was automatically send! Please do not answer to this message!";
 
 ?>
